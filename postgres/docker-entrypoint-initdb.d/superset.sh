aa21bfdab78d0ea0e87bc95bde5bb474da8125af
Joshua Barratt <joshua@barratt.pro>
2018-04-28 13:16:25 +0200

    more typos

--- postgres/docker-entrypoint-initdb.d/superset.sh
+++ postgres/docker-entrypoint-initdb.d/superset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
 	CREATE USER superset;
-	ALTER USER superset WITH PASSWORD '$POSTGRES_PASSWORD'
+	ALTER USER superset WITH PASSWORD '$POSTGRES_PASSWORD';
 	CREATE DATABASE superset;
 	GRANT ALL PRIVILEGES ON DATABASE superset TO superset;
 EOSQL
