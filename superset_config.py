169e97d2e6fc0ea65727e96194107110c0f6bfcc
Istvan Szabo <4446568+sknight80@users.noreply.github.com>
2018-05-24 12:32:53 -0400

    Update superset_config.py
    Added google oAuth setup

--- superset_config.py
+++ superset_config.py
@@ -1,5 +1,8 @@
 import os
 
+from flask_appbuilder.security.manager import AUTH_OID, AUTH_REMOTE_USER, AUTH_DB, AUTH_LDAP, AUTH_OAUTH
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 #MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY', '')
 # Set this API key to enable Mapbox visualizations
 MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY','')
@@ -43,3 +46,30 @@ WTF_CSRF_ENABLED = True
 # Add endpoints that need to be exempt from CSRF protection
 WTF_CSRF_EXEMPT_LIST = []
 
+#
+# Google oAuth
+#
+#CSRF_ENABLED = True
+
+#AUTH_TYPE = AUTH_OAUTH
+#AUTH_USER_REGISTRATION = True
+#AUTH_USER_REGISTRATION_ROLE = "Public"
+#OAUTH_PROVIDERS = [
+#    {
+#       'name': 'google',
+#       'whitelist': ['@company.com'],
+#       'icon': 'fa-google',
+#       'token_key': 'access_token', 
+#       'remote_app': {
+#            'base_url': 'https://www.googleapis.com/oauth2/v2/',
+#            'request_token_params': {
+#              'scope': 'email profile'
+#            },
+#            'request_token_url': None,
+#            'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+#            'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+#            'consumer_key': 'GOOGLE_AUTH_KEY',
+#            'consumer_secret': 'GOOGLE_AUTH_SECRET'
+#        }
+#    }
+#]
