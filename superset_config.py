ea4c39ba1a6a272bf9aef112b74a5d397c9b8157
mlgrm <o@mlgrm.net>
2018-06-08 12:09:22 +0200

    switched to container-optimised os

--- superset_config.py
+++ superset_config.py
@@ -51,25 +51,26 @@ WTF_CSRF_EXEMPT_LIST = []
 #
 #CSRF_ENABLED = True
 
-#AUTH_TYPE = AUTH_OAUTH
-#AUTH_USER_REGISTRATION = True
-#AUTH_USER_REGISTRATION_ROLE = "Public"
-#OAUTH_PROVIDERS = [
-#    {
-#       'name': 'google',
-#       'whitelist': ['@company.com'],
-#       'icon': 'fa-google',
-#       'token_key': 'access_token', 
-#       'remote_app': {
-#            'base_url': 'https://www.googleapis.com/oauth2/v2/',
-#            'request_token_params': {
-#              'scope': 'email profile'
-#            },
-#            'request_token_url': None,
-#            'access_token_url': 'https://accounts.google.com/o/oauth2/token',
-#            'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
-#            'consumer_key': 'GOOGLE_AUTH_KEY',
-#            'consumer_secret': 'GOOGLE_AUTH_SECRET'
-#        }
-#    }
-#]
+AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Public"
+OAUTH_PROVIDERS = [
+    {
+      'name': 'google',
+      'whitelist': os.getenv('GOOGLE_AUTH_WHITELIST',''),
+      'icon': 'fa-google',
+      'token_key': 'access_token',
+      'remote_app': {
+            'base_url': 'https://www.googleapis.com/oauth2/v2/',
+            'request_token_params': {
+             'scope': 'email profile'
+            },
+            'request_token_url': None,
+            'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+            'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+            'consumer_key': os.getenv('GOOGLE_AUTH_KEY',''),
+            'consumer_secret': os.getenv('GOOGLE_AUTH_SECRET','')
+        }
+    }
+]
+`.
